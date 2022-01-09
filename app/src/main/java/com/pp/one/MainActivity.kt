afe7fcae41a3cd49d960742a6fac4608cc0bd154
18571576617 <18571576617@163.com>
2022-01-09 16:02:24 +0800

    commit

--- app/src/main/java/com/pp/one/MainActivity.kt
+++ app/src/main/java/com/pp/one/MainActivity.kt
@@ -2,11 +2,14 @@ package com.pp.one
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.pp.putils.PUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.i("plq", "add: " + PUtils.add(1, 5));
     }
 }
